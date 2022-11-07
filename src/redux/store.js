76e5c8d127a15af35253d4d47120f11f452edad7
HarutHovsepyan <harut.hovsepyan.199419@gmail.com>
2022-11-07 14:55:55 +0400

    lesson redux file

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import {createStore} from "redux";
+
+
+const store = createStore(function(state, action) {
+  if (action.type === "edit-current-user-name") {
+    return {
+      ...state,
+      currentUser: {
+        name: action.payload.name
+      }
+    }
+  }
+  return state
+}, {
+  currentUser: {
+    name: "Harut"
+  }
+});
+
+
+export default store;
